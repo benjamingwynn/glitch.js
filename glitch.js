203216fcc24ae0807a73ab332d2bbde9f8ba4f91
Benjamin Gwynn <xenxier@gmail.com>
2014-07-27 18:57:55 +0100

    Update glitch.js to version 2.0
    + Multiple variations of glitch colours, allows for a more awesome effect.
    + Added the ability for glitch.js to distort multiple objects.
    + Added distort_only_text_inside_distort_objects, which is self-explanatory.
    - Removed glitch shapes which required CSS and didn't work very well.
    - Removed 'distort_text_multicolour' which allowed different text on the screen to be distorted in different colours, this looks 'more trippy than glitchy' and needed to be removed to allow multiple colour variations.

--- glitch.js
+++ glitch.js
@@ -1,94 +1,77 @@
-    /** 
-		* Xenxier's Glitch Script
-		*   (C) Benjamin Gwynn 2014 - http://xenxier.tk
-		*
-		**/
-		
-		// Glitch Configuration
-		var distort_colours = [ "#FD08FF", "#0AFF08" ];
-		var distort_object_name = 'body';
-		var distort_blur = 5;
-		var distort_chance = 6;
-		var distort_text = true;
-		var distort_text_types = ["h1", "h2", "h3", "p"];
-		var distort_text_multicolour = false;
-		var shape_chance = 4;
-		var speed = 50;
-		
-		var distort_object;
-		
-		window.onload = function start() {
-			glitch();
-			distort_object = document.getElementById(distort_object_name);
+/** 
+* glitch.js - Version 2.0
+*   (C) Benjamin Gwynn 2014 - http://xenxier.tk
+*
+**/
+
+/** Glitch Configuration */
+var distort_colours = [ [ "#FE57FF", "#FD08FF"], ["#58FF57", "#0AFF08"] ];
+var distort_objects = ['body'];
+var distort_blur = 5;
+var distort_chance = 6;
+var distort_text = true;
+var distort_text_types = ["h1", "h2", "h3", "p"];
+var speed = 50;
+var distort_only_text_inside_distort_objects = true;
+
+window.onload = function start() {
+	glitch();
+}
+
+function glitch() {
+	if (!distort_colour_number) {
+		var distort_colour_number = 0;
+	};
+	setInterval(function() {
+		for (var i = 0; i < distort_objects.length; i++) {
+			removeShadow(document.getElementById(distort_objects[i]));
 		}
-		
-		function glitch() {
-			setInterval(function() {
-				removeShadow(distort_object);
-				removeAllTextShadows();
-				//hideGlitchShapes();
-				if (getRandomNumber(0, shape_chance) == 0) {
-					//createGlitchShape(getRandomNumber(0, window.innerWidth), getRandomNumber(0, window.innerHeight), getRandomNumber(60, 150), getRandomNumber(20, 100));
-				}
-				if (getRandomNumber(0, distort_chance) == 0) {
-					colour = distort_colours[Math.floor((Math.random() * distort_colours.length))];
-					addRandomShadow(distort_object, colour)
-					if (distort_text == true) {
-						for (var i = 0; i < distort_text_types.length; i++) {
-							for (var l = 0; l < document.getElementsByTagName(distort_text_types[i]).length; l++) {
-								if (distort_text_multicolour == true) {
-									colour = distort_colours[Math.floor((Math.random() * distort_colours.length))];
-								}
-								addRandomTextShadow(document.getElementsByTagName(distort_text_types[i])[l], colour);
-							}
+		removeAllTextShadows();
+		if (getRandomNumber(0, distort_chance) == 0 || distort_colour_number > 0) {
+			distort_object_name = distort_objects[Math.floor((Math.random() * distort_objects.length))];
+			distort_object = document.getElementById(distort_object_name);
+			colour = distort_colours[distort_colour_number][Math.floor((Math.random() * distort_colours[distort_colour_number].length))];
+			addRandomShadow(distort_object, colour)
+			if (distort_text == true) {
+				for (var i = 0; i < distort_text_types.length; i++) {
+					for (var l = 0; l < document.getElementsByTagName(distort_text_types[i]).length; l++) {
+						if (document.getElementsByTagName(distort_text_types[i])[l].parentNode == distort_object && distort_only_text_inside_distort_objects) {
+							addRandomTextShadow(document.getElementsByTagName(distort_text_types[i])[l], colour);
+						} else if (!distort_only_text_inside_distort_objects) {
+							addRandomTextShadow(document.getElementsByTagName(distort_text_types[i])[l], colour);
 						}
 					}
 				}
-			}, speed);
-		}
-		
-		function addRandomShadow(distort_object, colour) {
-			distort_object.style.boxShadow=getRandomNumber(-3, 3) + "px " + getRandomNumber(-3, 3) + "px " + distort_blur + "px " + colour;
-		}
-		
-		function addRandomTextShadow(distort_object, colour) {
-			distort_object.style.textShadow=getRandomNumber(0, 3) + "px 0px 1px " + colour;
-		}
-		
-		function removeShadow(distort_object) {
-			distort_object.style.boxShadow="none";
-		}
-		
-		function removeAllTextShadows() {
-			for (var i = 0; i < distort_text_types.length; i++) {
-				for (var l = 0; l < document.getElementsByTagName(distort_text_types[i]).length; l++) {
-					document.getElementsByTagName(distort_text_types[i])[l].style.textShadow="none";
-				}
 			}
-		}
-		
-		function getRandomNumber(min, max) {
-			return Math.floor(Math.random()*(max-min+1)+min);
-		}
-		
-		function createGlitchShape(left, top, width, height) {
-			var shape;
-			if (getRandomNumber(0, 1) == 0) {
-				shape = document.getElementById('glitch');
+			if (distort_colour_number == distort_colours.length - 1) {
+				distort_colour_number = 0;
 			} else {
-				shape = document.getElementById('glitchCircle');
-				height = width; // Stop our circles from squishing.
+				distort_colour_number = distort_colour_number + 1;
 			}
-			
-			shape.style.left = left + "px";
-			shape.style.top = top + "px";
-			shape.style.width = width + "px";
-			shape.style.height = height + "px";
-			
-			shape.style.display = "visible";
 		}
-		
-		function hideGlitchShapes() {
-			document.getElementById('glitch').style.display = "none";
-			document.getElementById('glitchCircle').style.display = "none";
+	}, speed);
+}
+
+function addRandomShadow(distort_object, colour) {
+	distort_object.style.boxShadow=getRandomNumber(-3, 3) + "px " + getRandomNumber(-3, 3) + "px " + distort_blur + "px " + colour;
+}
+
+function addRandomTextShadow(distort_object, colour) {
+	distort_object.style.textShadow=getRandomNumber(0, 3) + "px 0px 1px " + colour;
+}
+
+function removeShadow(distort_object) {
+	distort_object.style.boxShadow="none";
+}
+
+function removeAllTextShadows() {
+	for (var i = 0; i < distort_text_types.length; i++) {
+		for (var l = 0; l < document.getElementsByTagName(distort_text_types[i]).length; l++) {
+			document.getElementsByTagName(distort_text_types[i])[l].style.textShadow="none";
 		}
+	}
+}
+
+function getRandomNumber(min, max) {
+	return Math.floor(Math.random()*(max-min+1)+min);
+}
