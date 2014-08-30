b7840c53ada444db7bfd3246a170add9611e0eb4
Benjamin Gwynn <xenxier@gmail.com>
2014-08-30 22:16:19 +0100

    Update glitch.js to version 3.0
    *** jQuery 1.x is now required (tested and working on version 1.11.1).
    + Distortion works using a specified class, rather than many ID's ('distort_class_name').
    + Added 'distort_divs', a boolean to toggle if divs of the glitch class should have a glitch effect.
    + Added a new 'distort_objects_one_at_a_time' boolean, which causes a random element of the glitch class to distort, rather than all of them distorting at once.
    + Added the 'stopGlitch()' function, to stop the glitch effect clock at any time.
    + Added distort_object_count, and distort_count for developer diagnostics.
    + Simple welcome message logging version to the browsers javascript console.
    * getRandomNumber is now used more often, rather than math shit everywhere.
    * Cleaned up code and added useful comments.
    * Colours other than the primary colours in the 'distort_colours' table should now work as they're supposed to.
    * Cleaned up the code behind 'distort_backwards' - functionality is still exactly the same.
    * 'speed' is now 'distort_speed' - functionality is still exactly the same.
    * A distorted div will no longer distort text inside it, every element must be assigned to the glitch class individually.
    - Removed 'distortable_objects' table, only one object class can be distorted.
    - Removed 'distort_text_objects' table, all text is distorted if it belongs to the glitch class.
    - Removed the automatic function to launch glitch() on window load. This now must be called by the hosting website.

--- glitch.js
+++ glitch.js
@@ -1,88 +1,115 @@
 /** 
-* glitch.js - Version 2.1
-*   (C) Benjamin Gwynn 2014 - http://xenxier.tk
+* glitch.js - Version 3.0
+*	A highly flexible glitch effect written in Javascript using jQuery and HTML5 shadows.
+*	(C) Benjamin Gwynn 2014 - http://xenxier.tk
 *
+*	~ Note: Now requires jQuery and functions differently as of 3.0 update ~
+*
+*	Integrate into your website as follows:
+*	<script>$(window).onload(function(){glitch();});</script>
+*
+*	You can then use glitch.js on any text object like this:
+*	<p class='glitch'>Sample glitch text</p>
+*
+*	...and any div using the following:
+*	<div class='glitch' style='width=100px; height=100px;'></div>
+*
+*	Enjoy!
 **/
 
 /** Glitch Configuration */
 var distort_colours = [ [ "#FE57FF", "#FD08FF"], ["#58FF57", "#0AFF08"] ];
-var distortable_objects = ['body'];
-var distort_distortable_objects = true;
+var distort_class_name = '.glitch';
 var distort_blur = 1;
 var distort_chance = 6;
-var distort_text = true;
-var distort_text_types = ["h1", "h2", "h3", "p"];
-var speed = 50;
-var distort_only_text_inside_distortable_objects = false;
+var distort_speed = 50;
 var distort_range = 6;
+var distort_divs = true;
+var distort_text = true;
 var distort_backwards = false;
+var distort_objects_one_at_a_time = false;
 
-if (distort_range_negative) {
-	var distort_range_negative = 0 - distort_range;
-} else {
-	var distort_range_negative = 0;
-}
-
-window.onload = function start() {
-	glitch();
-}
+/** Unconfigurable Variables */
+var glitchRefreshIntervalId;
+var distort_range_negative;
+var distort_count = 0;
+var distort_object_count = 0;
+var distort_colour_number = 0;
 
 function glitch() {
-	if (!distort_colour_number) {
-		var distort_colour_number = 0;
-	};
-	setInterval(function() {
-		for (var i = 0; i < distortable_objects.length; i++) {
-			removeShadow(document.getElementById(distortable_objects[i]));
-		}
-		removeAllTextShadows();
+	console.info("Loaded glitch.js version 3.0. Created by Xenxier (Benjamin Gwynn) - http://xenxier.tk");
+	
+	distort_object_count = $(distort_class_name).length;
+	
+	if (distort_backwards) {
+		distort_range_negative = 0 - distort_range;
+	} else {
+		distort_range_negative = 0;
+	}
+	
+	glitchRefreshIntervalId = setInterval(function() {
+		removeShadow(distort_class_name);
+		
+		// On a random chance (OR if the glitch effect needs to repeat to display the next colour in the colour sets table):
 		if (getRandomNumber(0, distort_chance) == 0 || distort_colour_number > 0) {
-			distort_object_name = distortable_objects[Math.floor((Math.random() * distortable_objects.length))];
-			distort_object = document.getElementById(distort_object_name);
-			colour = distort_colours[distort_colour_number][Math.floor((Math.random() * distort_colours[distort_colour_number].length))];
-			if (distort_distortable_objects) {
-				addRandomShadow(distort_object, colour)
+			distort_count++;
+			
+			// Get our distort object:
+			if (distort_objects_one_at_a_time) {
+				distort_object = $($(distort_class_name).get(getRandomNumber(0, $(distort_class_name).length - 1)));
+			} else {
+				distort_object = $(distort_class_name);
 			}
-			if (distort_text) {
-				for (var i = 0; i < distort_text_types.length; i++) {
-					for (var l = 0; l < document.getElementsByTagName(distort_text_types[i]).length; l++) {
-						if (document.getElementsByTagName(distort_text_types[i])[l].parentNode == distort_object && distort_only_text_inside_distortable_objects) {
-							addRandomTextShadow(document.getElementsByTagName(distort_text_types[i])[l], colour);
-						} else if (!distort_only_text_inside_distortable_objects) {
-							addRandomTextShadow(document.getElementsByTagName(distort_text_types[i])[l], colour);
-						}
+			
+			// Get a colour:
+			colour = distort_colours[distort_colour_number][getRandomNumber(0,distort_colours[distort_colour_number].length)];
+			
+			// Distort:
+			if (distort_objects_one_at_a_time) {
+				if ($(distort_object).is('div') && distort_divs) {
+					addRandomShadow(distort_object, colour);
+				} else if (distort_text) {
+					addRandomTextShadow(distort_object, colour);
+				}
+			} else {
+				for (var i = 0; i < $(distort_object).length; i++) {
+					var x = $($(distort_object).get(i));
+					if ($(x).is('div') && distort_divs) {
+						addRandomShadow(x, colour);
+					} else if (distort_text) {
+						addRandomTextShadow(x, colour);
 					}
 				}
 			}
+			
+			// Modify what colour we will have next:
 			if (distort_colour_number == distort_colours.length - 1) {
 				distort_colour_number = 0;
 			} else {
-				distort_colour_number = distort_colour_number + 1;
+				distort_colour_number++;
 			}
 		}
-	}, speed);
+	}, distort_speed);
 }
 
 function addRandomShadow(distort_object, colour) {
-	distort_object.style.boxShadow=getRandomNumber(distort_range_negative, distort_range) + "px " + getRandomNumber(distort_range_negative, distort_range) + "px " + distort_blur + "px " + colour;
+	$(distort_object).css('box-shadow', getRandomNumber(distort_range_negative, distort_range) + "px " + getRandomNumber(distort_range_negative, distort_range) + "px " + distort_blur + "px " + colour);
 }
 
 function addRandomTextShadow(distort_object, colour) {
-	distort_object.style.textShadow=getRandomNumber(distort_range_negative, distort_range) + "px 0px " + distort_blur + "px " + colour;
+	$(distort_object).css('text-shadow', getRandomNumber(distort_range_negative, distort_range) + "px 0px " + distort_blur + "px " + colour);
 }
 
-function removeShadow(distort_object) {
-	distort_object.style.boxShadow="none";
-}
-
-function removeAllTextShadows() {
-	for (var i = 0; i < distort_text_types.length; i++) {
-		for (var l = 0; l < document.getElementsByTagName(distort_text_types[i]).length; l++) {
-			document.getElementsByTagName(distort_text_types[i])[l].style.textShadow="none";
-		}
-	}
+function removeShadow(object) {
+	$(object).css('box-shadow', 'none');
+	$(object).css('text-shadow', 'none');
 }
 
 function getRandomNumber(min, max) {
-	return Math.floor(Math.random()*(max-min+1)+min);
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function stopGlitch() {
+	console.log("Stopping glitch.js now!");
+	clearInterval(glitchRefreshIntervalId);
 }
